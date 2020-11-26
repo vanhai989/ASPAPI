4194f593b9b352911e04d4b313793797006816af
vanhai989 <haidev989@gmail.com>
2020-11-27 00:43:54 +0700

    init

---
+++ API/API/Data/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    [Table("ProductTable")]
+    public class Product
+    {
+        [ForeignKey("Id")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ API/API/Data/ProductContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class ProductContextFactory : IDesignTimeDbContextFactory<ProductContext>
+    {
+        public ProductContext CreateDbContext(string[] args)
+        {
+            var builler = new DbContextOptionsBuilder<ProductContext>();
+            builler.UseSqlServer("Server =DESKTOP-15IPD1K; Database =ASP; Trusted_Connection = True; MultipleActiveResultSets = true");
+            return new ProductContext(builler.Options);
+        }
+    }
+}
